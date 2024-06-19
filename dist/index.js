544bbcd757439ef1213203b866d7a006bea1a27a
Joselu <joseluissgz97@gmail.com>
2024-06-19 20:19:42 +0200

    refactor: Update room list toggle functionality and styles

--- dist/index.js
+++ dist/index.js
@@ -5,22 +5,47 @@ const navMenu = document.querySelector('.header__nav');
 
 const roomListContainer = document.querySelector('.rooms-grid');
 const roomsList = document.querySelectorAll('.room');
-const toogleRoom = document.querySelector('.toogleRoom');
+const toogleRoomList = document.querySelector('.toogle-list');
+const toogleRoomGrid = document.querySelector('.toogle-grid');
 
 const toggleMenu = (type) => {
   if (type === 'open') {
+    toggleClose.classList.remove('header__toggle--hidden');
+    toggleActive.classList.add('header__toggle--hidden');
+
     navMenu.classList.add('header__nav--active');
   }
-  if (type === 'close') return navMenu.classList.remove('header__nav--active');
+  if (type === 'close') {
+    toggleActive.classList.remove('header__toggle--hidden');
+    toggleClose.classList.add('header__toggle--hidden');
+    return navMenu.classList.remove('header__nav--active');
+  }
 };
 
-if (toogleRoom) {
-  toogleRoom.addEventListener('click', () => {
-    roomListContainer.classList.remove('rooms-grid');
+if (toogleRoomList) {
+  toogleRoomList.addEventListener('click', () => {
+    toogleRoomList.classList.add('rooms__action--active');
+    toogleRoomGrid.classList.remove('rooms__action--active');
+
     roomListContainer.classList.add('rooms-grid--list');
 
     roomsList.forEach((room) => {
       room.classList.add('room--list');
+      //div-content
+      room.children[1].classList.add('room__content--list');
+    });
+  });
+}
+if (toogleRoomGrid) {
+  toogleRoomGrid.addEventListener('click', () => {
+    toogleRoomList.classList.remove('rooms__action--active');
+
+    toogleRoomGrid.classList.add('rooms__action--active');
+
+    roomListContainer.classList.remove('rooms-grid--list');
+
+    roomsList.forEach((room) => {
+      room.classList.remove('room--list');
     });
   });
 }
